c6a1198eb2f8b40a7171df666fec2acec0ef01c0
acampos3 <44051503+acampos3@users.noreply.github.com>
2019-11-18 15:26:04 +0100

    Create ejer8.java

---
+++ ejer8.java
@@ -0,0 +1,43 @@
+/*
+ * Crea la clase Coche con dos constructores. Uno no toma parámetros y
+ *  el otro sí. Los dos constructores inicializarán los atributos
+ *  marca y modelo de la clase. Crea dos objetos (cada objeto llama 
+ *  a un constructor distinto) y verifica que todo funciona correctamente.
+ */
+package ut3;
+import java.util.*;
+public class ejer8 {
+ public static void main(String[]args) {
+	 Scanner sc=new Scanner(System.in);
+	     Coche car1 = new Coche();
+		
+		car1.Mostrar();
+		
+		car1 = new Coche("Seat","Ateca");
+		
+		car1.Mostrar();
+	
+        
+     
+}
+}
+
+//*main
+package ut3;
+
+public class Coche {
+  private String marca;
+  private String modelo;
+   public Coche(){
+    	marca= "seat";
+        modelo="ibiza";
+    }
+   public Coche(String marca,String modelo){
+    	this.marca=marca;
+    	this.modelo=modelo;
+    }
+   public void Mostrar () {
+		System.out.println("marca: "+marca+" modelo :"+modelo);
+	}
+	
+}
